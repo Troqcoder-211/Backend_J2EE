d5fd0c8ee10605ac30c52939e36dd205a8316498
phucnguyen0704 <phucnguyen.07042004@gmail.com>
2025-10-17 11:02:12 +0700

    Done task 2.25-2.30

--- src/main/java/j2ee/ourteam/controllers/ConversationController.java
+++ src/main/java/j2ee/ourteam/controllers/ConversationController.java
@@ -8,6 +8,7 @@ import j2ee.ourteam.models.conversation.UpdateConversationDTO;
 import j2ee.ourteam.services.conversation.IConversationService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,37 +28,37 @@ public class ConversationController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ConversationDTO>> getAll(){
+    public ResponseEntity<List<ConversationDTO>> getAll(Authentication authentication){
         List<ConversationDTO> conversationDTOList = _conversationService.findAll();
         return ResponseEntity.ok(conversationDTOList);
     }
 
     @GetMapping("/findbyid/{uuid}")
-    public ResponseEntity<ConversationDTO> getById(@PathVariable UUID uuid){
+    public ResponseEntity<ConversationDTO> getById(@PathVariable UUID uuid, Authentication authentication){
         Optional<ConversationDTO> conversationDTO = _conversationService.findById(uuid);
         return conversationDTO.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<ConversationDTO> createConversation(@RequestBody CreateConversationDTO dto){
+    public ResponseEntity<ConversationDTO> createConversation(@RequestBody CreateConversationDTO dto, Authentication authentication){
         ConversationDTO created = _conversationService.create(dto);
         return ResponseEntity.ok(created);
     }
 
     @PatchMapping(path = "/update/{uuid}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ConversationDTO> updateConversation(@PathVariable UUID uuid, @RequestBody UpdateConversationDTO dto){
+    public ResponseEntity<ConversationDTO> updateConversation(@PathVariable UUID uuid, @RequestBody UpdateConversationDTO dto, Authentication authentication){
         ConversationDTO updated = _conversationService.update(uuid, dto);
         return ResponseEntity.ok(updated);
     }
 
     @DeleteMapping("/delete/{uuid}")
-    public ResponseEntity<Void> deleteConversation(@PathVariable UUID uuid){
+    public ResponseEntity<Void> deleteConversation(@PathVariable UUID uuid, Authentication authentication){
         _conversationService.deleteById(uuid);
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/archived/{uuid}")
-    public Boolean isArchived(@PathVariable UUID uuid, @RequestBody ArchivedConversationDTO dto){
+    public Boolean isArchived(@PathVariable UUID uuid, @RequestBody ArchivedConversationDTO dto, Authentication authentication){
         boolean Archived = _conversationService.isArchived(uuid, dto);
 
         return Archived;
